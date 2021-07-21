80942c046f2917ddeb3960e191105fb62bc0c012
= <=>
2021-07-21 11:45:30 +0700

    commit to res

---
+++ src/app/prd/listPrd.ts
@@ -0,0 +1,37 @@
+import { Product } from "src/models/product";
+
+
+export var fakeData: Product[] = [
+    {
+        id: 132,
+        name: "Cốc tự khuấy",
+        gia: 120000,
+    },
+    {
+        id: 231,
+        name: "Bình giữ nhiệt",
+        gia: 240000,
+    },
+    {
+        id: 231,
+        name: "Đèn pin siêu sáng",
+        gia: 120000,
+    },
+    {
+        id: 634,
+        name: "Đèn bàn",
+        gia: 26000,
+    },
+    {
+        id: 2341,
+        name: "Giấy vệ sinh",
+        gia: 85000,
+    },
+    {
+        id: 12456,
+        name: "Chai coca",
+        gia: 24000,
+    },
+
+    
+]
---
+++ src/app/prd/prd.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit, Output, EventEmitter, APP_ID } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { fakeData } from './listPrd';
+
+@Component({
+  selector: 'app-prd',
+  templateUrl: './prd.component.html',
+  styleUrls: ['./prd.component.css']
+})
+export class PrdComponent implements OnInit {
+
+  formData!: FormGroup;
+  list = fakeData;
+  constructor() { }
+  name: any;
+  Prodct = {id: 0, name: "", gia: 0}
+
+  ngOnInit(): void {
+    this.formData = new FormGroup({
+      id: new FormControl(Validators.required),
+      name: new FormControl(),
+      gia: new FormControl()
+    });
+    
+  }
+  
+  onSelect(){
+
+  }
+  currentID = 1;
+  removeItem(id: number) {
+    this.list.splice(id, 1)
+  }
+  add(){
+    this.list.push(this.formData.value)
+  }
+  edit(id: number){
+    this.Prodct = this.list[id]
+    
+  }
+  Search(){
+
+  }
+}
